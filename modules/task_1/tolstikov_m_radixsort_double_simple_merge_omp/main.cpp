37b496190379f5451444292af2ffe19fecd91ccc
Maksim <m.tolstikov98@mail.ru>
2019-03-16 18:34:18 +0300

    delete msd sort

--- modules/task_1/tolstikov_m_radixsort_double_simple_merge_omp/main.cpp
+++ modules/task_1/tolstikov_m_radixsort_double_simple_merge_omp/main.cpp
@@ -35,23 +35,30 @@ void CountingSort(double *inp, double *out, int byteNum, int size) {
     }
 }
 
+void PrintArray(double *array, int size) {
+    if (size < 15) {
+        for (int i = 0; i < size; i++) {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 void LSDSortDouble(double *inp, int size) {
     double *out = new double[size];
+    std::cout << std::endl;
     for (int i = 0; i < 8; i += 2) {
         CountingSort(inp, out, i, size);
+        std::cout << "Step " << i << "       ";
+        PrintArray(out, size);
         CountingSort(out, inp, i + 1, size);
+        std::cout << "Step " << i + 1 << "       ";
+        PrintArray(inp, size);
     }
+    std::cout << std::endl;
     delete[] out;
 }
 
-void MSDSortDouble(double *inp, int size) {
-    double *out = new double[size];
-    for (int i = 7; i >= 0; i -= 2) {
-        CountingSort(inp, out, i, size);
-        CountingSort(out, inp, i - 1, size);
-    }
-    delete[] out;
-}
 
 void CheckingSort(double *mas, int size) {
     for (int i = 0; i < size - 1; i++) {
@@ -63,15 +70,6 @@ void CheckingSort(double *mas, int size) {
     std::cout << "Sort is correcrtly" << std::endl;
 }
 
-void PrintArray(double *array, int size) {
-    if (size < 10) {
-        for (int i = 0; i < size; i++) {
-            std::cout << array[i] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
 void GenerateArray(double *mas, int size) {
     int b = 100;
     int a = 0;
@@ -80,22 +78,16 @@ void GenerateArray(double *mas, int size) {
     }
 }
 
-void CopyArray(double *mas, int size, double *tmpmas) {
-    for (int i = 0; i < size; i++)
-        tmpmas[i] = mas[i];
-}
-
 int main(int argc, char *argv[]) {
     double time_lsd = 0;
-    double time_msd = 0;
     int size = 9;
     std::srand((unsigned)time(NULL));
-    double *mas, *tmpmas;
+    double *mas;
     if (argc == 3)
         if (strcmp(argv[1], "-size") == 0)
             size = atoi(argv[2]);
     mas = new double[size];
-    tmpmas = new double[size];
+    std::cout << "Array: ";
     GenerateArray(mas, size);
 
     if (mas == NULL) {
@@ -103,22 +95,15 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    CopyArray(mas, size, tmpmas);
     PrintArray(mas, size);
     time_lsd = clock();
     LSDSortDouble(mas, size);
     time_lsd = (clock() - time_lsd) / static_cast<double>(CLOCKS_PER_SEC);
-    time_msd = clock();
-    MSDSortDouble(tmpmas, size);
-    time_msd = (clock() - time_msd) / static_cast<double>(CLOCKS_PER_SEC);
+    std::cout << "Array after LSD sort: ";
     PrintArray(mas, size);
-    PrintArray(tmpmas, size);
     std::cout << "LSD ";
     CheckingSort(mas, size);
-    std::cout << "MSD ";
-    CheckingSort(tmpmas, size);
     std::cout << "Execution time LSD sort: " << time_lsd << std::endl;
-    std::cout << "Execution time MSD sort: " << time_msd << std::endl;
 
     delete[] mas;
     return 0;
